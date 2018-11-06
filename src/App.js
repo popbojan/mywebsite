67234663e901f57a829b50e6d4e2eec01a272b9e
pop <bp@altapay.com>
2018-11-06 09:12:23 +0100

    [a1] Introduce navigation bar skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
     return (
       <div className="App">
-          <header>My Website</header>
+          <header></header>
+          <Navbar></Navbar>
       </div>
     );
 }
